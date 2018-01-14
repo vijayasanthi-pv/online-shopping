7f34bdc7339aa08eb296c1cb521353fc603bfa98
vijayasanthi <vijayasanthi.pv@gmail.com>
2018-01-14 00:34:10 -0800

    Successfully loaded static files

---
+++ onlineshopping/src/main/java/net/ecomm/onlineshopping/controller/PageController.java
@@ -0,0 +1,35 @@
+package net.ecomm.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","Welcome to SpringMVC");
+		return mv;
+	}
+	
+	/*@RequestMapping(value= {"/test"})
+	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting) {
+		if(greeting == null) {
+			greeting = "Default Msg";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
+	@RequestMapping(value= {"/test1/{greeting}"})
+	public ModelAndView test1(@PathVariable("greeting") String greeting) {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}*/
+}
