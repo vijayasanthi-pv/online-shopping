651f6432ff5e43be7bc011047c4bdc097178f9ce
vijayasanthi <vijayasanthi.pv@gmail.com>
2018-01-14 22:13:21 -0800

    Completed the interface theme and other issues

--- onlineshopping/src/main/java/net/ecomm/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/ecomm/onlineshopping/controller/PageController.java
@@ -12,7 +12,24 @@ public class PageController {
 	@RequestMapping(value= {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","Welcome to SpringMVC");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
+		return mv;
+	}
+	
+	@RequestMapping("/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
+		return mv;
+	}
+	
+	@RequestMapping("/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
 		return mv;
 	}
 	
---
+++ onlineshopping/src/main/webapp/WEB-INF/views/shared/footer.jsp
@@ -0,0 +1,7 @@
+<footer class="py-5 footer">
+	<div class="container">
+		<p class="m-0 text-center text-black">Copyright &copy; Your
+			Website 2017</p>
+	</div>
+	<!-- /.container -->
+</footer>
